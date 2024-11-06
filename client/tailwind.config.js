d19cc8da07a26ed47f94523a58e4f790ddfc81d9
francis-100 <mayiekaf@gmail.com>
2024-11-07 02:19:00 +0300

    admin page and client setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      primary: '#1D4ED8',
+      secondary: '#3B82F6',
+    },
+  },
+};
+export const plugins = [];
