e3766ebc11edcfd779c23912b28d0df2d288c7c7
francis-100 <mayiekaf@gmail.com>
2024-11-06 13:19:36 +0300

    boilerplate

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1D4ED8',
+        secondary: '#3B82F6',
+      },
+    },
+  },
+  plugins: [],
+}
---
+++ server/smart_exam_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmartExamAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'smart_exam_app'
